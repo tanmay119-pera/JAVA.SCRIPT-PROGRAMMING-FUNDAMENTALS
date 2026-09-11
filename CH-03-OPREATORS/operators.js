16d72143beb452c66d656c9394d9f2ce49c9624b
TANMAY <tanmay.w119@gmail.com>
2026-09-11 17:52:11 +0530

    ch-3

---
+++ CH-03-OPREATORS/operators.js
@@ -0,0 +1,15 @@
+//^                                               CHAPTER - 03 OPERATORS & CONDITIONAL STATEMENT IN JAVASCRIPT                                              //
+
+//& COMMENTS IN JAVA.SCRIPT
+
+/* COMMENTS ARE USED TO EXPLAIN THE CODE AND MAKE IT MORE READABLE. THEY ARE IGNORED BY THE JAVASCRIPT ENGINE DURING EXECUTION. 
+AND ALL TYPE WILL BE IGNORED IN JAVASCRIPT. */
+
+//^ 1. Single line comment
+// This is a single line comment in JavaScript
+
+//^ 2. Multi line comment
+/* 
+This is a multi-line comment in JavaScript.
+You can write multiple lines of comments here.
+*/
